0ad76347be44d53e149f11239f040c9cea1187e6
lohar7s1 <159903248+lohar7s1@users.noreply.github.com>
2024-02-24 10:20:51 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 7901
+let accountEmail = "anil@hotmail.com"
+var accountPassword = "12345"
+accountCity = "Ahmedabad"
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "anil.com"
+accountPaswword = "67890"
+accountCity = "Jaipur"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
