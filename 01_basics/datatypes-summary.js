1dcd04ca4eecb3be1cf002dfc55218007b375ab1
lohar7s1 <159903248+lohar7s1@users.noreply.github.com>
2024-02-29 14:16:11 +0000

    datatype summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -9,7 +9,29 @@ const isLoggedIn = false
 const  outsideTemp = null
 let userEmail;
 
+const id = Symbol("123")
+const anotherId = Symbol("123")
+
+console.log(id === anotherId);
+
+// const bigNumber = 125854644646878788454545n
+
+
 
 // Reference (Non primitive)
 
 // Array, Objects, Functions
+
+const heros = ["shaktiman", "batman", "ironman"]
+let myObj = {
+    name: "anil",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}  
+
+console.log(typeof anotherId);
+
+//https://ecma-international.org/publications-and-standards/standards/ecma-262/
