1c7616bcf71e1a4bac54aa9d0d0cefa090a4fb32
lohar7s1 <159903248+lohar7s1@users.noreply.github.com>
2024-02-29 04:44:07 +0000

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,15 @@
+// # Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, Bigint
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const  outsideTemp = null
+let userEmail;
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
